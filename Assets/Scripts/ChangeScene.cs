b3dc1f10ddeb0c04abdc569be09a58f408731c8e
Carlos <carloscreus@ceroca.cat>
2025-10-24 17:54:55 +0200

    Boton jugar y instrucciones redirige

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -29,4 +29,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
         SceneManager.LoadScene("classScene");
     }
 
+    public void InstructionsScene()
+    {
+        SceneManager.LoadScene("InstructionsScene");
+    }
+
 }
